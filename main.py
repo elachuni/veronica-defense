080cbe0530d05bf98930e025f101bf3ac323faac
manuq <manuq@manuq-laptop>
2010-04-20 00:03:59 -0300

    remove import not used

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyglet.resource
 from cocos.director import director
 
 import settings
-from logic import World, Level, ResourceManager
+from logic import Level
 from levels_data import levels_data
 from level_scene import LevelScene
 
@@ -13,18 +13,18 @@ def main():
     pyglet.resource.reindex()
     
     director.init(vsync=True)
-
+    
     # store window size in settings:
     settings.WINDOW_SIZE = director.get_window_size()
-
+    
     # just a test level for now:
     level_data = levels_data[0]
     level = Level(level_data)
-
+    
     # the level is shown as a cocos scene:
     level_scene = LevelScene(level)
     director.run(level_scene)
-    
+
 
 if __name__ == '__main__':
     main()
